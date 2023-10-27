07d9f52de5dbb57dd36efb5df0f003ecabfacd2c
GdGohan <70231641+GdGohan@users.noreply.github.com>
2023-10-27 20:23:56 -0300

    Add files via upload

--- customlangv2.py
+++ customlangv2.py
@@ -12,8 +12,8 @@ def read_language_from_txt():
 def rename_bckp_folder():
     language = read_language_from_txt()
     if language:
-        bckp_folder = os.path.join("modf/language", "custom")
-        os.rename(bckp_folder, os.path.join("modf/language", language))
+        bckp_folder = os.path.join("data/language", "Custom")
+        os.rename(bckp_folder, os.path.join("data/language", language))
         main()
 
 languages_list = None
@@ -31,12 +31,12 @@ def select_language(selected_language):
 def update_language_list(selected_item, selected_language):
     global previous_language, selected_language_label
 
-    selected_language_folder = os.path.join("modf/language", selected_item)
+    selected_language_folder = os.path.join("data/language", selected_item)
 
     if previous_language:
-        os.rename(os.path.join("modf/language", "custom"), previous_language)
+        os.rename(os.path.join("data/language", "Custom"), previous_language)
 
-    custom_path = os.path.join("modf/language", "custom")
+    custom_path = os.path.join("data/language", "Custom")
     os.rename(selected_language_folder, custom_path)
 
     previous_language = selected_language_folder
@@ -45,9 +45,9 @@ def update_language_list(selected_item, selected_language):
 
 def load_languages():
     languages_list.delete(0, tk.END)
-    language_folders = os.listdir("modf/language")
+    language_folders = os.listdir("data/language")
     for language in language_folders:
-        if os.path.isdir(os.path.join("modf/language", language)):
+        if os.path.isdir(os.path.join("data/language", language)):
             languages_list.insert(tk.END, language)
 
 def save_language_to_txt(selected_language):
@@ -57,7 +57,7 @@ def save_language_to_txt(selected_language):
 def main():
     global languages_list, previous_language, selected_language_label
     
-    custom_folder = os.path.join("modf/language", "custom")
+    custom_folder = os.path.join("data/language", "Custom")
     if os.path.exists(custom_folder):
         rename_bckp_folder()  # Renomeia a pasta "bckp" se o idioma estiver no arquivo de texto
     else:
@@ -69,7 +69,7 @@ def main():
         languages_list = tk.Listbox(window)
         languages_list.pack()
 
-        select_button = tk.Button(window, text="Select Language", command=lambda: select_language(selected_language))
+        select_button = tk.Button(window, text="Selecionar idioma", command=lambda: select_language(selected_language))
         select_button.pack()
 
         load_languages()
@@ -77,7 +77,7 @@ def main():
         selected_language_label = tk.Label(window, text="Custom")
         selected_language_label.pack()
 
-        custom_folder = os.path.join("modf/language", "custom")
+        custom_folder = os.path.join("data/language", "custom")
 
         window.mainloop()
 
--- patcher.py
+++ patcher.py
@@ -1,6 +1,6 @@
 import os
 
-def modify_exe_string(exe_path):
+def modify_exe_string(exe_path, custom_name):
     # Abre o arquivo EXE original para leitura de bytes binários
     with open(exe_path, 'rb') as exe_file:
         exe_data = exe_file.read()
@@ -11,16 +11,24 @@ def modify_exe_string(exe_path):
     # Substitui "config.ini" por "confmf.ini"
     modified_data2 = modified_data.replace(b'config.ini', b'confmf.ini')
 
+    # Converta o nome personalizado em bytes usando UTF-8
+    custom_name_bytes = custom_name.encode('utf-8')
+
+    # Substitui "custom" pelo nome personalizado fornecido pelo usuário
+    modified_data3 = modified_data2.replace(b'Custom', custom_name_bytes)
+
     # Cria um novo arquivo EXE com a string modificada
     new_exe_path = "kd2modlauncher.exe"
     with open(new_exe_path, 'wb') as new_exe_file:
-        new_exe_file.write(modified_data2)
+        new_exe_file.write(modified_data3)
 
     return new_exe_path
 
 def main():
     exe_path = "koumajou2.exe"
-    new_exe_path = modify_exe_string(exe_path)
+    custom_name = input("Write the name of the custom language: ")
+    new_exe_path = modify_exe_string(exe_path, custom_name)
+    print(f"The modified EXE has been created at {new_exe_path}")
 
 if __name__ == "__main__":
     main()
